bbdc1de8a4e44196b780d9468aa3613a2ec69d92
bob <marcellinus.cg@gmail.com>
2025-01-30 10:42:45 +0700

    initial commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,13 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends(
+    "next/core-web-vitals",
+    "next/typescript",
+    "standart",
+    "plugin:tailwindcss/recommended",
+    "prettier"
+  ),
 ];
 
 export default eslintConfig;
---
+++ lib/ratelimit.ts
@@ -0,0 +1,12 @@
+import redis from "@/database/redis";
+import { Ratelimit } from "@upstash/ratelimit";
+
+// Create a new ratelimiter, that allows 10 requests per 10 seconds
+const ratelimit = new Ratelimit({
+  redis,
+  limiter: Ratelimit.slidingWindow(5, "1m"),
+  analytics: true,
+  prefix: "@upstash/ratelimit",
+});
+
+export default ratelimit;
